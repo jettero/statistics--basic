63cdb6be4fb0339fbcf8f60c5c067c9dbca93cf0
Paul Miller <paul@voltar.org>
2008-08-21 13:10:09 -0400

    self defined computers is better than a new class

--- Basic.pm
+++ Basic.pm
@@ -15,7 +15,6 @@ use Statistics::Basic::Mode;
 use Statistics::Basic::StdDev;
 use Statistics::Basic::Variance;
 use Statistics::Basic::Vector;
-use Statistics::Basic::ComputedVector;
 
 our $VERSION = "1.5";
 our $fmt = new Number::Format;
@@ -40,8 +39,6 @@ our @EXPORT_OK   = (qw(
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 1;
-
-sub cvec     { my $r = eval { ref($_[0]) ? Statistics::Basic::ComputedVector->new( $_[0] )   : Statistics::Basic::ComputedVector->new( [@_] );   }; croak $@ if $@; $r }
 
 sub vector   { my $r = eval { ref($_[0]) ? Statistics::Basic::Vector->new( $_[0] )   : Statistics::Basic::Vector->new( [@_] );   }; croak $@ if $@; $r }
 sub mean     { my $r = eval { ref($_[0]) ? Statistics::Basic::Mean->new( $_[0] )     : Statistics::Basic::Mean->new( [@_] );     }; croak $@ if $@; $r }
--- t/08_cvec.t
+++ t/08_cvec.t
@@ -3,6 +3,17 @@ use strict;
 use Test;
 use Statistics::Basic;
 
-plan tests => 1;
+plan tests => 4;
 
-my $cvec = eval { Statistics::Basic::ComputedVector->new([1 .. 3]) };
+my $i = eval { Statistics::Basic::Vector->new([ 1 .. 30 ]) };
+my $j = $i->copy;
+
+$i->set_computer( my_computer => sub { $j->set_vector([ grep {$_<= 3} $i->query ]) });
+
+ok( $j->size, 3 );
+do { local $ENV{DEBUG}=0; ok( $j, "[1, 2, 3]" ); };
+
+$i->insert( 2 );
+
+ok( $j->size, 3 );
+do { local $ENV{DEBUG}=0; ok( $j, "[2, 3, 2]" ); };
