77a997e772070d68936db84295e55a383ef3545c
Paul Miller <paul@voltar.org>
2008-08-22 21:29:33 -0400

    forgot to version up, as usual

--- Basic.pm
+++ Basic.pm
@@ -17,7 +17,7 @@ use Statistics::Basic::Variance;
 use Statistics::Basic::Vector;
 use Statistics::Basic::ComputedVector;
 
-our $VERSION = "1.5";
+our $VERSION = "1.53";
 our $fmt = new Number::Format;
 
 $ENV{DEBUG} ||= 0;
