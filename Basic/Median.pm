363574737ee0b29e449b466bc54437b15f3a2cad
Paul Miller <jettero@cpan.org>
2009-06-24 06:40:11 -0400

    converted median to _ovb

--- Basic/Median.pm
+++ Basic/Median.pm
@@ -5,22 +5,12 @@ use strict;
 use warnings;
 use Carp;
 
-use Statistics::Basic;
+use base 'Statistics::Basic::_OneVectorBase';
 
-use overload
-    '""' => sub { defined( my $v = $_[0]->query ) || return "n/a"; $Statistics::Basic::fmt->format_number("$v", $ENV{IPRES}) },
-    '0+' => sub { $_[0]->query },
-    ( exists($ENV{TOLER}) ? ('==' => sub { abs($_[0]-$_[1])<=$ENV{TOLER} }) : () ),
-    'eq' => sub { "$_[0]" eq "$_[1]" },
-    fallback => 1; # tries to do what it would have done if this wasn't present.
-
-1;
-
-# new {{{
 sub new {
     my $class = shift;
 
-    warn "[new median]\n" if $ENV{DEBUG} >= 2;
+    warn "[new $class]\n" if $ENV{DEBUG} >= 2;
 
     my $this   = bless {}, $class;
     my $vector = eval { Statistics::Basic::Vector->new(@_) }; croak $@ if $@;
@@ -32,15 +22,14 @@ sub new {
 
     return $this;
 }
-# }}}
-# _recalc {{{
+
 sub _recalc {
     my $this = shift;
     my $v = $this->{v};
     my $cardinality = $v->size;
 
     delete $this->{recalc_needed};
-    delete $this->{median};
+    delete $this->{_value};
     return unless $cardinality > 0;
     return unless $v->query_filled; # only applicable in certain circumstances
 
@@ -48,81 +37,13 @@ sub _recalc {
     my $center = int($cardinality/2);
 
     if ($cardinality%2) {
-        $this->{median} = $v[$center];
+        $this->{_value} = $v[$center];
 
     } else {
-        $this->{median} = ($v[$center] + $v[$center-1])/2;
+        $this->{_value} = ($v[$center] + $v[$center-1])/2;
     }
 
-    warn "[recalc median] vector[int($cardinality/2)] = $this->{median}\n" if $ENV{DEBUG};
+    warn "[recalc " . ref($this) . "] vector[int($cardinality/2)] = $this->{_value}\n" if $ENV{DEBUG};
 }
-# }}}
-# _recalc_needed {{{
-sub _recalc_needed {
-    my $this = shift;
-       $this->{recalc_needed} = 1;
 
-    warn "[recalc_needed median]\n" if $ENV{DEBUG};
-}
-# }}}
-# query {{{
-sub query {
-    my $this = shift;
-
-    $this->_recalc if $this->{recalc_needed};
-
-    warn "[query median $this->{median}]\n" if $ENV{DEBUG};
-
-    return $this->{median};
-}
-# }}}
-# query_vector {{{
-sub query_vector {
-    my $this = shift;
-
-    return $this->{v};
-}
-# }}}
-
-# size {{{
-sub size {
-    my $this = shift;
-
-    return $this->{v}->size;
-}
-# }}}
-# set_size {{{
-sub set_size {
-    my $this = shift;
-    my $size = shift;
-
-    eval { $this->{v}->set_size($size) }; croak $@ if $@;
-}
-# }}}
-# set_vector {{{
-sub set_vector {
-    my $this = shift;
-
-    warn "[set_vector median]\n" if $ENV{DEBUG};
-
-    $this->{v}->set_vector(@_);
-}
-# }}}
-# insert {{{
-sub insert {
-    my $this = shift;
-
-    warn "[insert median]\n" if $ENV{DEBUG};
-
-    $this->{v}->insert(@_);
-}
-# }}}
-# ginsert {{{
-sub ginsert {
-    my $this = shift;
-
-    warn "[ginsert median]\n" if $ENV{DEBUG};
-
-    $this->{v}->ginsert(@_);
-}
-# }}}
+1;
